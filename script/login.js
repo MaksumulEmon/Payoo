4e72aa6a141cd5b65b1600bcb3fe4cc8fc3f4542
MaksumulEmon <maksumulemon300@gmail.com>
2026-02-18 10:16:56 +0600

    Log in Page done

--- script/login.js
+++ script/login.js
@@ -2,5 +2,23 @@
 
 document.getElementById('login-btn').addEventListener("click" , function(){
     //Get the mobile number
-    
+    const numberInput =document.getElementById("input-number");
+    const contractNumber = numberInput.value ;
+    console.log(contractNumber);
+
+
+    const inputin =document.getElementById("input-pin");
+    const pin = inputin.value ;
+    console.log(pin);
+
+
+    if(contractNumber =="01234567890" && pin =="1234"){
+        alert("Log in Sucessfully")
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    }else{
+        alert("Log in Failed");
+        return;
+    }
+
 })
